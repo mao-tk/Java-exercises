421dfce24238da0b713f03f530e4f64a2394f83d
mao-tk <x.ri.mm.s@gmail.com>
2023-09-11 17:07:28 +0900

    練習2-3(nameが日本語だと???になる)

--- src/App.java
+++ src/App.java
@@ -1,14 +1,15 @@
 public class App {
     public static void main(String[] args)  {
-        boolean isExpired = true;
-        char name = '駆';
-        double pi = 3.14;
-        long number = 314159265853979L;
-        String message = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
-        System.out.println(isExpired);
-        System.out.println(name);
-        System.out.println(pi);
-        System.out.println(number);
-        System.out.println(message);
+        System.out.println("ようこそ占いの館へ");
+        System.out.println("あなたの名前を入力してください");
+        String name = new java.util.Scanner(System.in).nextLine();
+        System.out.println("あなたの年齢を入力してください");
+        String ageString = new java.util.Scanner(System.in).nextLine();
+        int age = Integer.parseInt(ageString);
+        int fortune = new java.util.Random().nextInt(4);
+        fortune++;
+        System.out.println("占いの結果が出ました！");
+        System.out.println(age+"歳の"+name+"さん、あなたの運気番号は"+fortune+"です");
+        System.out.println("1:大吉 2:中吉 3:吉 4:凶");
     }
 }
