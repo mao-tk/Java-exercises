d7eab1aa19626d98f7455c3efed578702c9c2cdf
mao-tk <x.ri.mm.s@gmail.com>
2023-09-11 16:37:47 +0900

    練習1-3

--- src/App.java
+++ src/App.java
@@ -1,8 +1,14 @@
 public class App {
     public static void main(String[] args)  {
-        int a = 3;
-        int b = 5;
-        int c = a*b;
-        System.out.println("縦幅" + a + "横幅" + b + "の長方形の面積は、" + c);
+        boolean isExpired = true;
+        char name = '駆';
+        double pi = 3.14;
+        long number = 314159265853979L;
+        String message = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+        System.out.println(isExpired);
+        System.out.println(name);
+        System.out.println(pi);
+        System.out.println(number);
+        System.out.println(message);
     }
 }
