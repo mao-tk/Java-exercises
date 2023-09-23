521249e46c9354017da16f41f8825e70b05cad3f
mao-tk <x.ri.mm.s@gmail.com>
2023-09-23 15:55:30 +0900

    while文サンプル

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,9 @@
 public class Main {
   public static void main (String[] args) {
-    System.out.println("あなたの運勢を占います");
-    int fortune = new java.util.Random().nextInt(5)+1;
-    switch(fortune) {
-      case 1:
-      case 2:
-        System.out.println("いいね！");
-        break;
-      case 3:
-        System.out.println("普通です");
-        break;
-      case 4:
-      case 5:
-        System.out.println("うーん…");
-    }
+    int temp = 27;
+    while (temp > 25) {
+      temp--;
+      System.out.println("温度を1度下げました");
+  }
   }
 }
