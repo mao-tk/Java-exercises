9c1a5e879ef63f1e48eb2db397cc21982350c4e3
mao-tk <x.ri.mm.s@gmail.com>
2023-09-23 11:33:51 +0900

    コード3-2（繰り返し）

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 public class Main {
   public static void main(String[] args){
-    boolean tenki = false;
-    if (tenki == true) {
-      System.out.println("洗濯をします");
-      System.out.println("散歩にいきます");
-    } else {
-      System.out.println("DVDを見ます");
+    boolean door = false;
+    while (door == true) {
+      System.out.println("ノックする");
+      System.out.println("1分待つ");
     }
   }
 }
