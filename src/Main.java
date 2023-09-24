4f26d0a834c8d6a3b7b39dbbede0a33d60d0643f
mao-tk <x.ri.mm.s@gmail.com>
2023-09-24 13:19:27 +0900

    コード3-9　九九表示

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
-  public static void main(String[] args){
-    for(int i=0; i<3; i++){
-      System.out.print("現在" + (i+1) + "周目⇒");
+  public static void main(String[] args) {
+    for (int i = 1; i < 10; i++) {
+      for (int j = 1; j < 10; j++) {
+        System.out.print(i * j + " "); //掛け算結果
+      }
+      System.out.println(""); //段ごとに改行する
     }
   }
 }
