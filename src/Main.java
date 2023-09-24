ddacffc2b7703e78fb528d4e3103ac8cab509025
mao-tk <x.ri.mm.s@gmail.com>
2023-09-24 13:45:44 +0900

    練習3-3

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 public class Main {
   public static void main(String[] args) {
-    for (int i = 1; i < 10; i++) {
-      for (int j = 1; j < 10; j++) {
-        System.out.print(i * j + " "); //掛け算結果
-      }
-      System.out.println(""); //段ごとに改行する
+    int isHungry = 1;
+    String food = "おにぎり";
+    System.out.println("こんにちわ");
+    if (isHungry == 0) {
+      System.out.println("おなかがいっぱいです");
+    } else {
+      System.out.println("はらぺこです");
+      System.out.println(food + "をいただきます");
     }
+    System.out.println("ごちそう様でした");
   }
 }
